0c58726e04d863e30870965d25e2ae197790883c
jaeygun <jaeygun@gmail.com>
2024-01-17 22:59:01 +0900

    게시글 등록 개발

--- src/main/java/com/jaeygun/board/board/controller/BoardController.java
+++ src/main/java/com/jaeygun/board/board/controller/BoardController.java
@@ -6,7 +6,6 @@ import com.jaeygun.board.board.dto.ReplyDTO;
 import com.jaeygun.board.board.dto.ReplyLikeCheckDTO;
 import com.jaeygun.board.board.service.BoardService;
 import com.jaeygun.board.board.service.ReplyService;
-import com.jaeygun.board.common.dto.MessageDTO;
 import com.jaeygun.board.common.dto.PaginationDTO;
 import com.jaeygun.board.user.dto.UserDTO;
 import com.jaeygun.board.util.ClientUtil;
@@ -21,7 +20,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -109,30 +107,23 @@ public class BoardController {
     }
 
     @PostMapping("/save")
-    public void save(HttpServletRequest request, HttpServletResponse response, HttpSession session, @RequestBody BoardDTO boardDTO) throws ServletException, IOException {
+    public Map<String, Object> save(HttpServletRequest request, HttpServletResponse response, HttpSession session, BoardDTO boardDTO) throws ServletException, IOException {
 
         UserDTO loginUser = (UserDTO) session.getAttribute("loginUser");
-        MessageDTO messageDTO = new MessageDTO();
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put(JsonUtil.RESULT, JsonUtil.FAILURE);
 
         try {
             boardDTO = boardService.addPost(loginUser, boardDTO);
             log.info("[post add success] user : " + loginUser.getName() + ", subject : " + boardDTO.getSubject());
+            resultMap.put(ClientUtil.MESSAGE, "등록되었습니다.");
+            resultMap.put("redirectUrl", "/community/post/" + boardDTO.getBoardUid());
+            resultMap.put(JsonUtil.RESULT, JsonUtil.SUCCESS);
         } catch (Exception e) {
+            resultMap.put(ClientUtil.MESSAGE, "게시글 등록 중 오류가 발생했습니다.");
             log.error("An error occurred while registering the post.", e);
-            messageDTO.setMessage("게시글 등록 중 에러가 발생했습니다.\n잠시후 다시 시도해주세요.");
-            request.setAttribute("messageDTO", messageDTO);
-
-            RequestDispatcher requestDispatehcer = request.getRequestDispatcher("/messageRedirect");
-            requestDispatehcer.forward(request, response);
-            return;
         }
-
-        messageDTO.setMessage("등록되었습니다.");
-        messageDTO.setRedirectUri("/community/post/" + boardDTO.getBoardUid());
-        request.setAttribute("messageDTO", messageDTO);
-
-        RequestDispatcher requestDispatehcer = request.getRequestDispatcher("/messageRedirect");
-        requestDispatehcer.forward(request, response);
+        return resultMap;
     }
 
     @PostMapping("/{boardUid}/reply/add")
